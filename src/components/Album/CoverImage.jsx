12022e1dfadf55e69f2e2f082730e3af54c5f551
A R <12204041+iravella@users.noreply.github.com>
2023-10-21 19:18:31 +0200

    add hoverStyle

--- src/components/Album/CoverImage.jsx
+++ src/components/Album/CoverImage.jsx
@@ -1,10 +1,25 @@
-export const CoverImage = ({coverImage  }) => {
-   
-  
-    return (
-      <div className="image">
-      
-       <img src={coverImage} alt="Album Cover" />
+import HeartIcon from '../../assets/icons/heart.svg';
+import PlayIcon from '../../assets/icons/play.svg';
+import DotsIcon from '../../assets/icons/dots.svg';
+
+export function CoverImage({ coverImage }) {
+  return (
+    <div className="image-with-icons-wrapper">
+      <img src={coverImage} alt="Main Image" className="main-image" />
+
+      <div className="icon-container">
+        <button type="button" className="icon-button heart-button">
+          <img src={HeartIcon} alt="Like Icon" className="icon heart-icon" />
+        </button>
+
+        <button type="button" className="icon-button play-button">
+          <img src={PlayIcon}  alt="Play Icon" className="icon play-icon" />
+        </button>
+
+        <button type="button" className="icon-button dots-button">
+          <img src={DotsIcon} alt="Dots Icon" className="icon dots-icon" />
+        </button>
       </div>
-    );
-  };
+    </div>
+  );
+}

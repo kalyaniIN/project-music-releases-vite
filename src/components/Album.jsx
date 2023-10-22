375bf2a27c74a15b5dd3e9c0ad3ff31943724fdd
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-10-22 16:25:56 +0200

    Links for AlbumName and ArtistName are implemented

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -3,19 +3,35 @@ import { ArtistName } from "./Album/ArtistName";
 import { CoverImage } from "./Album/CoverImage";
 
 export const Album = ({ album }) => {
-  const { name, artists, images, external_url } = album;
+  const { name, artists, images, external_urls } = album;
   const artistNames = artists.map((artist) => artist.name).join(", ");
+
+  const openSpotifyLink = (url) => {
+    window.open(url, "_blank");
+  };
 
   return (
     <div className="album">
       <div className="image-container">
         <CoverImage coverImage={images[1].url} />
       </div>
-      <a href={external_url} className="album-name">
+      <a
+        href={external_urls.spotify}
+        className="album-name"
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={() => openSpotifyLink(external_urls.spotify)}
+      >
         <AlbumName albumName={name} />
       </a>
       <div className="artists">
-        <a href={external_url} className="artist-name">
+        <a
+          href={artists[0].external_urls.spotify}
+          className="artist-name"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => openSpotifyLink(artists[0].external_urls.spotify)}
+        >
           <ArtistName artistName={artistNames} />
         </a>
       </div>

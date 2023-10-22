29c6752787f7b69bfb5e23b74b826202ed349805
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-10-22 15:23:03 +0200

    Icons for Netlify Version

--- src/components/Album/ButtonsContainer.jsx
+++ src/components/Album/ButtonsContainer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const ButtonsContainer = () => {
     return (
         <div className="buttons-container">
-            <img className="icon heart" src="src\assets\icons\heart.svg" alt="heart-icon" />
-            <img className="icon play" src="src\assets\icons\play.svg" alt="play-icon" />
-            <img className="icon dot" src="src\assets\icons\dots.svg" alt="dots-icon" />
+            <img className="icon heart" src="/heart.svg" alt="heart-icon" />
+            <img className="icon play" src="/play.svg" alt="play-icon" />
+            <img className="icon dot" src="/dots.svg" alt="dots-icon" />
         </div>
     );
 };
